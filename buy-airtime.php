8f2d9633cc138b0eef7f775f1feca5b41dc82616
root <root@localhost.localdomain>
2019-05-13 19:11:17 +0100

    Airtime purchase page

---
+++ buy-airtime.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+<head>
+
+<?php require('main-header.php'); ?>
+</head>
+
+<body class="page-user">
+
+    <!-- .topbar-wrap -->
+    <div class="page-header">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-xl-7 text-center">
+                    <h2 class="page-title">Buy airtime</h2>
+                </div>
+            </div>
+        </div>
+        <!-- .container -->
+    </div>
+    <div class="page-content">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="content-area card">
+                        <div class="card-innr card-innr-fix">
+                            <form action="#" class="validate-modern wizard-wrap wizard-simple">
+                                <div class="wizard-head d-none">
+                                    <h5>Step 1</h5>
+                                </div>
+                                <div class="wizard-content">
+                                    <div class="content-area card">
+                                        <div class="card-innr">
+                                            <div class="card-head">
+                                                <h4 class="card-title">Select Network</h4>
+                                            </div>
+                                            <div class="token-currency-choose">
+                                                <div class="row guttar-15px">
+                                                    <div class="col-6">
+                                                        <div class="pay-option">
+                                                            <input class="pay-option-check" type="radio" value="MTN" id="payeth" name="network" checked><label class="pay-option-label" for="payeth"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-eth"></em><span
+                                                                        class="pay-cur">MTN</span></span>
+                                                                    </label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option">
+                                                            <input class="pay-option-check" type="radio" value="AIRTEL" id="paylte" name="network"><label class="pay-option-label" for="paylte"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-ltc"></em><span
+                                                                        class="pay-cur">AIRTEL</span></span>
+                                                                    </label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option">
+                                                            <input class="pay-option-check" type="radio" value="GLO" id="paybtc" name="network"><label class="pay-option-label" for="paybtc"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-btc"></em><span
+                                                                        class="pay-cur">GLO</span></span>
+                                                                    </label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option">
+                                                            <input class="pay-option-check" type="radio" value="9MOBILE" id="payusd" name="network"><label class="pay-option-label" for="payusd"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon fas fa-dollar-sign"></em><span
+                                                                        class="pay-cur">9MOBILE</span></span>
+                                                                </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- .row -->
+                                            </div>
+                                            <!-- info -->
+                                            <!-- <div class="pay-notes">
+                                                <div class="note note-plane note-light note-md font-italic"><em
+                                                        class="fas fa-info-circle"></em>
+                                                    <p>Tokens will appear in your account after payment successfully made and approved
+                                                        by our team. <br class="d-none d-lg-block"> Please note that, TWZ tokens will
+                                                        distributed end of ICO Token Sales. </p>
+                                                </div>
+                                            </div> -->
+                                        </div>
+                                        <!-- .card-innr -->
+                                    </div>
+                                </div>
+                                <div class="wizard-head d-none">
+                                    <h5>Step 2</h5>
+                                </div>
+                                <div class="wizard-content">
+                                    <div class="content-area card">
+                                        <div class="card-innr">
+                                            <div class="card-head">
+                                                <h4 class="card-title">Select Amount</h4>
+                                            </div>
+                                            <div class="token-currency-choose">
+                                                <div class="row guttar-15px">
+                                                    <div class="col-6">
+                                                        <div class="pay-option">
+                                                            <input class="pay-option-check" type="radio" id="payeth" name="payOption" checked><label class="pay-option-label" for="payeth"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-eth"></em><span
+                                                                        class="pay-cur">MTN</span></span><span
+                                                                    class="pay-amount">100.00</span></label></div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option"><input class="pay-option-check" type="radio" id="paylte" name="payOption"><label class="pay-option-label" for="paylte"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-ltc"></em><span
+                                                                        class="pay-cur">MTN</span></span><span
+                                                                    class="pay-amount">200.00</span></label></div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option"><input class="pay-option-check" type="radio" id="paybtc" name="payOption"><label class="pay-option-label" for="paybtc"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon cf cf-btc"></em><span
+                                                                        class="pay-cur">MTN</span></span><span
+                                                                    class="pay-amount">500.00</span></label></div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="pay-option"><input class="pay-option-check" type="radio" id="payusd" name="payOption"><label class="pay-option-label" for="payusd"><span
+                                                                    class="pay-title"><em
+                                                                        class="pay-icon fas fa-dollar-sign"></em><span
+                                                                        class="pay-cur">MTN</span></span><span
+                                                                    class="pay-amount">1000.00</span></label></div>
+                                                    </div>
+                                                </div>
+                                                <!-- .row -->
+                                            </div>
+                                        </div>
+                                        <!-- .card-innr -->
+                                    </div>
+                                </div>
+                                <div class="wizard-head d-none">
+                                    <h5>Step 3</h5>
+                                </div>
+                                <div class="wizard-content">
+                                    <div class="input-item input-with-label"><label for="token-address" class="input-item-label">Your Wallet </label>
+                                        <div class="input-wrap"><input class="input-bordered required" data-msg="Required" type="text" id="token-address" name="token-address">
+                                        </div>
+                                    </div>
+                                    <!-- .input-item --><label class="input-item-label">I want to
+                                        contribute</label>
+                                    <ul class="d-flex flex-wrap checkbox-list">
+                                        <li>
+                                            <div class="input-item text-left"><input class="input-radio input-radio-sm required" data-msg="Required" id="lt1eth" name="cry-opt" type="radio"><label for="lt1eth">Less
+                                                    than 1 ETH</label></div>
+                                        </li>
+                                        <li>
+                                            <div class="input-item text-left"><input class="input-radio input-radio-sm required" data-msg="Required" id="ov1eth" name="cry-opt" type="radio"><label for="ov1eth">Over
+                                                    than 1 ETH</label></div>
+                                        </li>
+                                    </ul>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="input-item input-with-label"><label for="telegram-username" class="input-item-label">Telegram Username</label>
+                                                <div class="input-wrap"><input class="input-bordered required" data-msg="Required" type="text" id="telegram-username" name="telegram-username"></div>
+                                            </div>
+                                            <!-- .input-item -->
+                                        </div>
+                                        <!-- .col -->
+                                    </div>
+                                    <!-- .row -->
+                                </div>
+                            </form>
+                        </div>
+                        <!-- .card-innr -->
+                    </div>
+                    <!-- .card -->
+                </div>
+            </div>
+        </div>
+        <!-- .container -->
+    </div>
+    <!-- .page-content -->
+    <div class="footer-bar">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-8">
+                    <ul class="footer-links">
+                        <li><a href="#">Whitepaper</a></li>
+                        <li><a href="faq-page.html">FAQs</a></li>
+                        <li><a href="regular-page.html">Privacy Policy</a></li>
+                        <li><a href="regular-page.html">Terms of Condition</a></li>
+                    </ul>
+                </div>
+                <!-- .col -->
+                <div class="col-md-4 mt-2 mt-sm-0">
+                    <div class="d-flex justify-content-between justify-content-md-end align-items-center guttar-25px pdt-0-5x pdb-0-5x">
+                        <div class="copyright-text">&copy; 2018 TokenWiz.</div>
+                        <div class="lang-switch relative"><a href="#" class="lang-switch-btn toggle-tigger">En <em
+                                    class="ti ti-angle-up"></em></a>
+                            <div class="toggle-class dropdown-content dropdown-content-up">
+                                <ul class="lang-list">
+                                    <li><a href="#">Fr</a></li>
+                                    <li><a href="#">Bn</a></li>
+                                    <li><a href="#">Lt</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- .col -->
+            </div>
+            <!-- .row -->
+        </div>
+        <!-- .container -->
+    </div>
+    <!-- .footer-bar -->
+    <!-- JavaScript (include all script here) -->
+    <script src="assets/js/jquery.bundle49f7.js"></script>
+    <script src="assets/js/script49f7.js"></script>
+</body>
+<!-- Mirrored from demo.themenio.com/tokenwiz/form-wizard.html by HTTrack Website Copier/3.x [XR&CO'2017], Thu, 09 May 2019 11:29:12 GMT -->
+
+</html>
