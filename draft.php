2d5fe988d49854103aeaa530589df105721c1069
root <root@localhost.localdomain>
2019-05-13 19:13:19 +0100

    Draft codes :octocat:

---
+++ draft.php
@@ -0,0 +1,40 @@
+<?php
+    // Email exist //
+    // $a = emailExist('eagle@gmail.com');
+    // if($a == true){
+    //     echo 'email exist';
+    // }else{
+    //     echo "does not exist";
+    // }
+ 
+    //REgistration //
+
+    // $a = registerUser('Asaolu','Elijah','eagleee@gmail.com','incorrect');
+    // if ($a == 'email') {
+    //     echo "success";
+    // }elseif($a == 'exist') {
+    //     echo "email exist";
+    // }else {
+    //     echo 'error';
+    // }
+
+    // Login; //
+    // $app = new App;
+    // $a = $app->login('eagle@gmail.com','incorrect');
+    // if ($a == 'success') {
+    //     echo "Login success";
+    // }else{
+    //     echo "Failed";
+    // }
+
+    // Email exit; //
+    // $a = new App;
+    // $a = $a->emailExist('eaglopwee@gmail.com');
+    // if($a == true){
+    //     echo 'email exist';
+    // }else{
+    //     echo "does not exist";
+    // }
+?>
+
+    
