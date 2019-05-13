137aecf3b9a6b85adadccb2509d16fa773df56e4
root <root@localhost.localdomain>
2019-05-13 19:14:00 +0100

    Main header file

---
+++ main-header.php
@@ -0,0 +1,53 @@
+
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="images/favicon.png"><!-- Site Title  -->
+    <title>Priceless | Buy data and airtime</title><!-- Vendor Bundle CSS -->
+    <link rel="stylesheet" href="assets/css/vendor.bundle49f7.css"><!-- Custom styles for this template -->
+    <link rel="stylesheet" href="assets/css/style.css" id="layoutstyle">
+
+    <div class="topbar-wrap">
+        <div class="topbar is-sticky">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center">
+                    <ul class="topbar-nav d-lg-none">
+                        <li class="topbar-nav-item relative"><a class="toggle-nav" href="#">
+                                <div class="toggle-icon"><span class="toggle-line"></span><span
+                                        class="toggle-line"></span><span class="toggle-line"></span><span
+                                        class="toggle-line"></span></div>
+                            </a></li><!-- .topbar-nav-item -->
+                    </ul><!-- .topbar-nav --><a class="topbar-logo" href="index.html">
+                        <img src="images/logo-light2x.png" srcset="images/logo-light2x.png 2x" alt="logo"></a>
+                    <ul class="topbar-nav">
+                        <li class="topbar-nav-item relative"><span
+                                class="user-welcome d-none d-lg-inline-block">Asaolu Elijah</span><a
+                                class="toggle-tigger user-thumb" href="#"><em class="ti ti-user"></em></a>
+                        </li><!-- .topbar-nav-item -->
+                    </ul><!-- .topbar-nav -->
+                </div>
+            </div><!-- .container -->
+        </div><!-- .topbar -->
+        <div class="navbar">
+            <div class="container">
+                <div class="navbar-innr">
+                    <ul class="navbar-menu">
+                        <li><a href="index-2.html"><em class="ikon ikon-user"></em> Home</a></li>
+                        <li><a href="buy-token.html"><em class="ikon ikon-coins"></em> About</a></li>
+                        <li><a href="ico-distribution.html"><em class="ikon ikon-dashboard"></em> Dashboard</a></li>
+                        <li><a href="transactions.html"><em class="ikon ikon-distribution"></em> Buy Airtime</a></li>
+                        <li><a href="profile.html"><em class="ikon ikon-distribution"></em> Buy Data</a></li>
+                        <li><a href="profile.html"><em class="ikon ikon-transactions"></em> Send/Recieve</a></li>
+                    </ul>
+                    <ul class="navbar-btns">
+                        <li><a href="kyc-application.html" class="btn btn-sm btn-outline btn-light"><em
+                                    class="text-primary ti ti-mobile"></em><span>Priceless Mobile</span></a></li>
+                        <!-- <li class="d-none"><span class="badge badge-outline badge-success badge-lg"><em
+                                    class="text-success ti ti-files mgr-1x"></em><span class="text-success">KYC
+                                    Approved</span></span>
+                        </li> -->
+                    </ul>
+                </div><!-- .navbar-innr -->
+            </div><!-- .container -->
+        </div><!-- .navbar -->
+    </div><!-- .topbar-wrap -->
