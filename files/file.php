ffd5f9138fb13abf06570f5cc8440e6fee54f88c
root <root@localhost.localdomain>
2019-05-13 19:06:22 +0100

    Draft files for json tutorial

---
+++ files/file.php
@@ -0,0 +1,16 @@
+<?php
+// $file = fopen('text.html','w');
+// $text = "<h3>Hello wow</h3> \n";
+// fwrite($file,$text);
+// $text = "<h3>Crazy</h3>";
+// fwrite($file,$text);
+// fclose($file);
+?>
+<script>
+    var employees = [
+    {"firstName":"John", "lastName":"Doe"},
+    {"firstName":"Anna", "lastName":"Smith"},
+    {"firstName":"Peter","lastName": "Jones"}
+];
+console.log(employees[1].firstName);
+</script>
